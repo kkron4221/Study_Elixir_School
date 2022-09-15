0aa5f91fd4771b8ffa3654ba2e7b76be8928e78d
ricky <42535443+kkron4221@users.noreply.github.com>
2022-09-15 10:27:27 +0900

    Study Process Monitoring and Task

--- sandbox/mix.exs
+++ sandbox/mix.exs
@@ -63,6 +63,24 @@ end
     end
   end
 
+
+  # Process Monitoring
+  # defmodule Example do
+  #   def explode, do: exit(:kaboom)
+
+  #   def run do
+  #     {pid, ref} = spawn_monitor(Example, :explode, [])
+
+  #     receive do
+  #       {:DOWN, _ref, :process, _from_pid, reason} -> IO.puts("Exit reason: #{reason}")
+  #     end
+  #   end
+  # end
+
+  # Task
   defmodule Example do
-    def explode, do: exit(:kaboom)
+    def double(x) do
+      :timer.sleep(2000)
+      x * 2
+    end
   end
