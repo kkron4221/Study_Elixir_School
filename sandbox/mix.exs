835a07773c4af79f8daa5cb55ef513a0f029334e
ricky <42535443+kkron4221@users.noreply.github.com>
2022-09-16 08:00:37 +0900

    Study GenServer and Synchronous Functions

--- sandbox/mix.exs
+++ sandbox/mix.exs
@@ -84,3 +84,56 @@ end
       x * 2
     end
   end
+
+
+  # # GenServer
+  # defmodule SimpleQueue do
+  #   use GenServer
+
+  #   @doc """
+  #   キューを開始し、リンクします。
+  #   これはヘルパー関数です。
+  #   """
+
+  #   def start_link(state \\ []) do
+  #     GenServer.start_link(__MODULE__, state, neme: __MODULE__)
+  #   end
+
+  #   @doc """
+  #   GenServer.init/1コールバック
+  #   """
+
+  #   def init(state), do: {:ok, state}
+  # end
+
+  defmodule SimpleQueue do
+    use GenServer
+
+    ### GenServer API
+
+    @doc """
+    GenServer.init/1コールバック
+    """
+    def init(state), do: {:ok, state}
+
+    @doc """
+    GenServer.handle_call/3コールバック
+    """
+
+    def handle_call(:dequeue, __from, [value | state]) do
+      {:reply, value, state}
+    end
+
+    def handle_call(:dequeue, __from, []), do: {:reply, nil, []}
+
+    def handle_call(:queue, _from, state), do: {:reply, state, state}
+
+    ### クライアント側API / ヘルパー関数
+
+    def start_link(state \\ []) do
+      GenServer.start_link(__MODULE__, state, name: __MODULE__)
+    end
+
+    def queue, do: GenServer.call(__MODULE__, :queue)
+    def dequeue, do: GenServer.call(__MODULE__, :dequeue)
+  end
